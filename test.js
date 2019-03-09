d6d7dca2efebcc2513568814d7f3cabcbaabbb61
angiegigishang <shangxingyu@163.com>
2019-03-09 16:42:56 +0800

    8个触点完成

--- test.js
+++ test.js
@@ -4,6 +4,10 @@ window.onload = function() {
 	var mainDiv = document.getElementById("main");
 	var leftDiv = document.getElementById("left");
 	var downDiv = document.getElementById("down");
+	var leftup = document.getElementById("left-up");
+	var rightup = document.getElementById("right-up");
+	var leftdown = document.getElementById("left-down");
+	var rightdown = document.getElementById("right-down");
 	var ifKeyDown = false;
 	var contact = "";
 
@@ -27,6 +31,26 @@ window.onload = function() {
 		contact = "down";
 	}
 
+	leftup.onmousedown = function() {
+		ifKeyDown = true;
+		contact = "left-up"
+	}
+
+	rightup.onmousedown = function() {
+		ifKeyDown = true;
+		contact = "right-up"
+	}
+
+	leftdown.onmousedown = function() {
+		ifKeyDown = true;
+		contact = "left-down";
+	}
+
+	rightdown.onmousedown = function() {
+		ifKeyDown = true;
+		contact = "right-down";
+	}
+
 	window.onmouseup = function() {
 		ifKeyDown = false;
 	}
@@ -34,35 +58,65 @@ window.onload = function() {
 	window.onmousemove = function (e) {
 		if(ifKeyDown == true) {
 			if(contact == "right") {
-				var x = e.clientX;
-				var addWidth  = "";
-				var widthBefore = mainDiv.clientWidth -2; 
-				addWidth = x - getPosition(mainDiv).left - widthBefore;
-				mainDiv.style.width = addWidth + widthBefore + "px";
+				rightMove(e);
 			} else if (contact == "up") {
-				var y = e.clientY;
-				var mainY = getPosition(mainDiv).top;
-				var addHeight = mainY - y;
-				var heightBefore = mainDiv.offsetHeight - 2;
-				mainDiv.style.height = heightBefore + addHeight + "px";
-				mainDiv.style.top = mainDiv.offsetTop - addHeight + "px";
+				upMove(e); 
 			} else if (contact == "left") {
-				var x = e.clientX;
-				var mainX = getPosition(mainDiv).left;
-				var addWidth = mainX - x;
-				var widthBefore = mainDiv.offsetWidth - 2;
-				mainDiv.style.width = widthBefore + addWidth + "px";
-				mainDiv.style.left = mainDiv.offsetLeft - addWidth + "px";
+				leftMove(e);
 			} else if (contact == "down") {
-				var y = e.clientY;
-				var heightBefore = mainDiv.offsetHeight - 2;
-				var mainY = getPosition(mainDiv).top;
-				var addHeight = y - mainY - heightBefore;
-				mainDiv.style.height = heightBefore + addHeight + "px";
-			}		
+				downMove(e);
+			} else if (contact == "left-up") {
+				upMove(e); 
+				leftMove(e);
+			} else if (contact == "right-up") {
+				rightMove(e);
+				upMove(e); 
+			} else if (contact == "left-down") {
+				leftMove(e);
+				downMove(e);
+			} else if (contact == "right-down") {
+				rightMove(e);
+				downMove(e);
+			}	
 		}	
+	}
+
+	function rightMove (e) {
+	    var x = e.clientX;
+		var addWidth  = "";
+		var widthBefore = mainDiv.clientWidth -2; 
+		addWidth = x - getPosition(mainDiv).left - widthBefore;
+		mainDiv.style.width = addWidth + widthBefore + "px";
+	}
+
+	function upMove (e) {
+		var y = e.clientY;
+		var mainY = getPosition(mainDiv).top;
+		var addHeight = mainY - y;
+		var heightBefore = mainDiv.offsetHeight - 2;
+		mainDiv.style.height = heightBefore + addHeight + "px";
+		mainDiv.style.top = mainDiv.offsetTop - addHeight + "px";
+	}
+
+	function leftMove (e) {
+		var x = e.clientX;
+		var mainX = getPosition(mainDiv).left;
+		var addWidth = mainX - x;
+		var widthBefore = mainDiv.offsetWidth - 2;
+		mainDiv.style.width = widthBefore + addWidth + "px";
+		mainDiv.style.left = mainDiv.offsetLeft - addWidth + "px";
+	}
+
+	function downMove (e) {
+		var y = e.clientY;
+		var heightBefore = mainDiv.offsetHeight - 2;
+		var mainY = getPosition(mainDiv).top;
+		var addHeight = y - mainY - heightBefore;
+		mainDiv.style.height = heightBefore + addHeight + "px";
 	}	
 }
+
+
 
 
 function getPosition (node) {
